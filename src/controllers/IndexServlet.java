31a105ec03833d51eac7ca19d37e0d6e4dcaa368
yuuki.kitazawa <northyk0314@gmail.com>
2023-01-04 12:45:34 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -41,6 +41,12 @@ public class IndexServlet extends HttpServlet {
         em.close();
 
         request.setAttribute("messages", messages);
+
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request,  response);
     }
